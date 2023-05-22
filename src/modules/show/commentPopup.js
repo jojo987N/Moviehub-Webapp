cff27d602b76973c9c1e15aed3b097e3a56968e2
binodbhusal <124040422+binodbhusal@users.noreply.github.com>
2023-05-22 19:43:40 +0100

    Add commentPopup class

--- src/modules/show/commentPopup.js
+++ src/modules/show/commentPopup.js
@@ -4,5 +4,24 @@ export default class CommentPopup {
     this.nameInput = null;
     this.commentTextArea = null;
   }
-  
+
+  async loadPopup() {
+    const showDetails = await this.fetchShowDetails();
+    const popupContainer = document.createElement('div');
+    popupContainer.classList.add('pop-container');
+    const showName = document.createElement('h3');
+    showName.textContent = showDetails.name;
+    popupContainer.appendChild(showName);
+    this.nameInput = document.createElement('input');
+    this.nameInput.type = 'text';
+    this.nameInput.placeholder = 'Your Name';
+    popupContainer.appendChild(this.nameInput);
+    
+  }
+
+  async fetchShowDetails() {
+    const response = await fetch(`https://api.tvmaze.com/shows/${this.selectedItem}`);
+    const showDetails = response.json();
+    return showDetails;
+  }
 }
