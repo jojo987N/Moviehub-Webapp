b1d8b4037d372586c2836382ff6207b72425caad
jemba narcisse <cabliveer@gmail.com>
2023-05-22 13:18:03 +0200

    Remove comments from index file

--- src/index.js
+++ src/index.js
@@ -5,13 +5,6 @@ import Show from './modules/show/show.js';
 import Store from './modules/store.js';
 
 (async () => {
-  //   const show = new Show({image:'ggg', name:'ggg', language:'ggg4', genre:'ggg', days:'ggg', time:'ggg', likes:'ggg'})
-  //   console.log(show.image)
   const store = new Store();
-
   document.querySelector('.content').innerHTML = (await store.shows).map(v => (new Show(v)).render()).join('');
-
-  // console.log((await store.shows).map(v => new Show(v)))
-
-  // console.log(new Show({a:1, image: 4}))
 })();
