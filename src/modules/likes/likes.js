887e15582c123a7b3494e645604fa5ed88ce9de3
jemba narcisse <cabliveer@gmail.com>
2023-05-23 15:28:25 +0200

    Likes add method return promise

--- src/modules/likes/likes.js
+++ src/modules/likes/likes.js
@@ -1,15 +1,13 @@
 import URL_BASE from '../global.js';
 
 export default class Likes {
-    static add = (like) => {
-      fetch(`${URL_BASE}apps/${localStorage.getItem('app')}/likes/`, {
-        method: 'POST',
-        body: JSON.stringify(like),
-        headers: {
-          'Content-type': 'application/json; charset=UTF-8',
-        },
-      });
-    }
+    static add = (like) => fetch(`${URL_BASE}apps/${localStorage.getItem('app')}/likes/`, {
+      method: 'POST',
+      body: JSON.stringify(like),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    });
 
     static store() {
       return (async () => await (await fetch(`${URL_BASE}apps/${localStorage.getItem('app')}/likes/`)).json())();
