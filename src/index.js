2a9b2c257321b7c6805ddcfc755e74a34a735f41
jemba narcisse <cabliveer@gmail.com>
2023-05-23 15:51:56 +0200

    Solve linter error

--- src/index.js
+++ src/index.js
@@ -5,10 +5,9 @@ import Show from './modules/show/show.js';
 import Store from './modules/store.js';
 import App from './modules/app.js';
 import Likes from './modules/likes/likes.js';
-import init from './modules/utils';
+import init from './modules/utils.js';
 
 import CommentPopup from './modules/show/commentPopup.js';
-
 
 (async () => {
   const app = localStorage.getItem('app') || await (new App()).id;
