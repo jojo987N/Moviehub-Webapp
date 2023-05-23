bf714c7cfe16cf5ceffd0f32830819dc157a4f7e
jemba narcisse <cabliveer@gmail.com>
2023-05-23 05:06:59 +0200

    Add class to get unique identifier for the app

--- src/index.js
+++ src/index.js
@@ -3,8 +3,13 @@ import './style.scss';
 import './style.css';
 import Show from './modules/show/show.js';
 import Store from './modules/store.js';
+import App from './modules/app.js';
+import Likes from './modules/likes/likes.js';
 
 (async () => {
+  const app = localStorage.getItem('app') || await (new App()).id;
+  localStorage.setItem('app', app);
+  const likes = await Likes.store();
   const store = new Store();
-  document.querySelector('.content').innerHTML = (await store.shows).map(v => (new Show(v)).render()).join('');
+  document.querySelector('.content').innerHTML = (await store.shows).map(v => (new Show(v)).render(likes)).join('');
 })();
