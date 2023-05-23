ffdf0b4071e42daec65a8d97f619ecc1d96a5ef9
jemba narcisse <cabliveer@gmail.com>
2023-05-23 05:10:08 +0200

    Add likes in show class in render method

--- src/modules/show/show.js
+++ src/modules/show/show.js
@@ -2,8 +2,9 @@ import './show.scss';
 
 export default class Show {
   constructor({
-    image, name, language, genres, schedule,
+    id, image, name, language, genres, schedule,
   }) {
+    this.id = id;
     this.image = image;
     this.name = name;
     this.language = language;
@@ -11,7 +12,7 @@ export default class Show {
     this.schedule = schedule;
   }
 
-  render() {
+  render(likes) {
     return `<div>
         <div>
             <img src="${this.image.medium}"/>
@@ -20,7 +21,7 @@ export default class Show {
               <span>${this.name}</span>
               <div class="likes">
                   <i class="fa fa-heart-o" aria-hidden="true"></i>
-                  <span>5 likes</span>
+                  <span>${(likes.find((like) => like.item_id === this.id) || []).likes || 0} likes</span>
               </div>
             </div>
         <div class="buttonContainer">
