a352fe8cf120bdec4117039866353ef729c6b155
jemba narcisse <cabliveer@gmail.com>
2023-05-23 09:06:29 +0200

    Delete utils file

--- src/modules/utils.js
+++
@@ -1,8 +0,0 @@
-import Likes from "./likes/likes";
-import Show from "./show/show";
-
-export default async function display(store) {
-    const likes = await Likes.store();
-    document.querySelector('.content').innerHTML = (await store.shows).map(v => (new Show(v)).render(likes)).join('');
-
-}
