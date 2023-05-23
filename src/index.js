7f2a5685f968a4c2905f8240ee2d1caec076c23a
jemba narcisse <cabliveer@gmail.com>
2023-05-23 15:13:42 +0200

    Add init function in index file

--- src/index.js
+++ src/index.js
@@ -5,10 +5,12 @@ import Show from './modules/show/show.js';
 import Store from './modules/store.js';
 import App from './modules/app.js';
 import Likes from './modules/likes/likes.js';
+import init from './modules/utils';
 
 (async () => {
   const app = localStorage.getItem('app') || await (new App()).id;
   localStorage.setItem('app', app);
+  await init();
   const likes = await Likes.store();
   const store = new Store();
   document.querySelector('.content').innerHTML = (await store.shows).map(v => (new Show(v)).render(likes)).join('');
