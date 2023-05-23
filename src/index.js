3a9f058d4655bbeba7a4c843a2c8c6d5a4f7e710
jemba narcisse <cabliveer@gmail.com>
2023-05-23 13:14:14 +0200

    Add display test for item counter

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import Store from './modules/store.js';
 import App from './modules/app.js';
 import Likes from './modules/likes/likes.js';
 import Like from './modules/like/like.js';
+import ItemCounter from './modules/itemsCounter';
 
 (async () => {
   const app = localStorage.getItem('app') || await (new App()).id;
   localStorage.setItem('app', app);
   const store = new Store();
   await store.display();
+  console.log(ItemCounter.count())
   document.querySelectorAll('.show').forEach((show, i) => {
     show.querySelector('.like').onclick = () => {
       Likes.add(new Like(i + 1));
