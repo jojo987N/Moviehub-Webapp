caec3dd42014c6fbc4674d973b6cd9512a12a2e7
jemba narcisse <cabliveer@gmail.com>
2023-05-24 07:14:00 +0200

    Remove display method and unused imports

--- src/modules/store.js
+++ src/modules/store.js
@@ -1,17 +1,9 @@
-import Likes from './likes/likes.js';
-import Show from './show/show.js';
-
 export default class Store {
   constructor() {
     this._shows = fetch('https://api.tvmaze.com/shows');
   }
 
   get shows() {
-    return (async () => (await (await this._shows).json()).slice(0, 6))();
-  }
-
-  async display() {
-    const likes = await Likes.store();
-    document.querySelector('.content').innerHTML = (await this.shows).map((v) => (new Show(v)).render(likes)).join('');
+    return (async () => await (await this._shows).json())();
   }
 }
