b7fe0d3cdefaa592e8c6199e22ef58c5c76d3e29
jemba narcisse <cabliveer@gmail.com>
2023-05-24 07:08:57 +0200

    Create display function and add it in utils file

--- src/index.js
+++ src/index.js
@@ -3,26 +3,18 @@ import './style.scss';
 import './style.css';
 import Store from './modules/store.js';
 import App from './modules/app.js';
-import Likes from './modules/likes/likes.js';
-import Like from './modules/like/like.js';
-import ItemCounter from './modules/itemsCounter';
-import init from './modules/utils.js';
+import init, { display } from './modules/utils.js';
 import CommentPopup from './modules/show/commentPopup.js';
+import { DEFAULT_NB_ITEMS } from './modules/global.js';
 
 (async () => {
   const app = localStorage.getItem('app') || await (new App()).id;
   localStorage.setItem('app', app);
   await init();
   const store = new Store();
-  await store.display();
-  console.log(ItemCounter.count())
-  document.querySelectorAll('.show').forEach((show, i) => {
-    show.querySelector('.like').onclick = () => {
-      Likes.add(new Like(i + 1));
-      const likes = show.querySelector('.likes-nb');
-      likes.innerHTML = parseInt(likes.innerHTML, 10) + 1;
-    };
-  });
+  const shows = await store.shows;
+  const count = DEFAULT_NB_ITEMS;
+  await display(shows, count);
 })();
 
 document.addEventListener('click', (event) => {
